a038ff3366e3ba5e2d9c6817066fe742926ef886
Saucetray <justinsostreofficial@gmail.com>
2018-01-27 23:40:21 -0500

    Initial Commit.

---
+++ Assets/Actions/Asteroid_Movements.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid_Movements : MonoBehaviour {
+
+	private int asteroidSpeed; private int damage;
+
+
+	// Use this for initialization
+	void Start () {
+
+		asteroidSpeed = 10;
+		damage = 12;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		moveAsteroid ();
+
+	}
+
+	void moveAsteroid () {
+
+		gameObject.GetComponent<Rigidbody2D> ().velocity =
+			new Vector2 (0, -asteroidSpeed);
+
+	}
+
+	void onCollisionEnter2D(Collision2D col) {
+
+		Debug.Log ("Player has collided with " + col.collider.name);
+	
+	}
+
+}
---
+++ Assets/Actions/PlutoMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlutoMovement : MonoBehaviour {
+
+    private int movespeed, size;
+    private double sizeMult;
+    private float moveXDir, moveYDir;
+
+    private int frameCounter;
+
+	// Use this for initialization
+	void Start () {
+        movespeed = 5;
+        size = 1;
+        sizeMult = 1.0;
+        frameCounter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movePluto();
+        frameCounter++;
+	}
+
+    void movePluto()
+    {
+        moveXDir = Input.GetAxis("Vertical");
+        moveYDir = Input.GetAxis("Horizontal");
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveXDir * movespeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveYDir * movespeed, gameObject.GetComponent<Rigidbody2D>().velocity.x);
+    }
+}
